6d381e3c2637724d75fa0c9d6d46c2a184d00d40
Max <p353019433@gmail.com>
2026-08-17 17:36:49 +0800

    fix: silence notify logs in bilibili-json.js (no functional change)

--- scripts/bilibili-json.js
+++ scripts/bilibili-json.js
@@ -1,14 +1,13 @@
-const url = $request.url;
+﻿const url = $request.url;
 const method = $request.method;
 const notifyTitle = "bilibili-json";
-console.log(`b站json-2023.10.22`);
+console.log(`b绔檍son-2023.10.22`);
 if (!$response.body) {
-    // 有undefined的情况
-    console.log(`$response.body为undefined:${url}`);
+    // 鏈塽ndefined鐨勬儏鍐?    console.log(`$response.body涓簎ndefined:${url}`);
     $done({});
 }
 if (method !== "GET") {
-    $notification.post(notifyTitle, "method错误:", method);
+    console.log(notifyTitle, "method閿欒:", method);
 }
 let body = JSON.parse($response.body);
 
@@ -16,44 +15,42 @@ let body = JSON.parse($response.body);
 if (!body.data) {
     console.log(url);
     console.log(`body:${$response.body}`);
-    $notification.post(notifyTitle, url, "data字段错误");
+    console.log(notifyTitle, url, "data瀛楁閿欒");
 } else {
     if (url.includes("x/v2/splash")) {
-        console.log('开屏页' + (url.includes("splash/show") ? 'show' : 'list'));
+        console.log('寮€灞忛〉' + (url.includes("splash/show") ? 'show' : 'list'));
         if (!body.data.show) {
-            // 有时候返回的数据没有show字段
-            console.log('数据无show字段');
+            // 鏈夋椂鍊欒繑鍥炵殑鏁版嵁娌℃湁show瀛楁
+            console.log('鏁版嵁鏃爏how瀛楁');
         } else {
             delete body.data.show;
-            console.log('成功');
+            console.log('鎴愬姛');
         }
     } else if (url.includes("resource/show/tab/v2")) {
-        console.log('tab修改');
-        // 顶部右上角
-        if (!body.data.top) {
+        console.log('tab淇敼');
+        // 椤堕儴鍙充笂瑙?        if (!body.data.top) {
             console.log(`body:${$response.body}`);
-            $notification.post(notifyTitle, 'tab', "top字段错误");
+            console.log(notifyTitle, 'tab', "top瀛楁閿欒");
         } else {
             body.data.top = body.data.top.filter(item => {
-                if (item.name === '游戏中心') {
-                    console.log('去除右上角游戏中心');
+                if (item.name === '娓告垙涓績') {
+                    console.log('鍘婚櫎鍙充笂瑙掓父鎴忎腑蹇?);
                     return false;
                 }
                 return true;
             });
             fixPos(body.data.top);
         }
-        // 底部tab栏
-        if (!body.data.bottom) {
+        // 搴曢儴tab鏍?        if (!body.data.bottom) {
             console.log(`body:${$response.body}`);
-            $notification.post(notifyTitle, 'tab', "bottom字段错误");
+            console.log(notifyTitle, 'tab', "bottom瀛楁閿欒");
         } else {
             body.data.bottom = body.data.bottom.filter(item => {
-                if (item.name === '发布') {
-                    console.log('去除发布');
+                if (item.name === '鍙戝竷') {
+                    console.log('鍘婚櫎鍙戝竷');
                     return false;
-                } else if (item.name === '会员购' || item.tab_id === '会员购Bottom') {
-                    console.log('去除会员购');
+                } else if (item.name === '浼氬憳璐? || item.tab_id === '浼氬憳璐瑽ottom') {
+                    console.log('鍘婚櫎浼氬憳璐?);
                     return false;
                 }
                 return true;
@@ -61,10 +58,10 @@ if (!body.data) {
             fixPos(body.data.bottom);
         }
     } else if (url.includes("x/v2/feed/index")) {
-        console.log('推荐页');
+        console.log('鎺ㄨ崘椤?);
         if (!body.data.items?.length) {
             console.log(`body:${$response.body}`);
-            $notification.post(notifyTitle, '推荐页', "items字段错误");
+            console.log(notifyTitle, '鎺ㄨ崘椤?, "items瀛楁閿欒");
         } else {
             body.data.items = body.data.items.filter(i => {
                 const {card_type: cardType, card_goto: cardGoto} = i;
@@ -72,40 +69,40 @@ if (!body.data) {
                     if (cardType === 'banner_v8' && cardGoto === 'banner') {
                         if (!i.banner_item) {
                             console.log(`body:${$response.body}`);
-                            $notification.post(notifyTitle, '推荐页', "banner_item错误");
+                            console.log(notifyTitle, '鎺ㄨ崘椤?, "banner_item閿欒");
                         } else {
                             for (const v of i.banner_item) {
                                 if (!v.type) {
                                     console.log(`body:${$response.body}`);
-                                    $notification.post(notifyTitle, '推荐页', "type错误");
+                                    console.log(notifyTitle, '鎺ㄨ崘椤?, "type閿欒");
                                 } else {
                                     if (v.type === 'ad') {
-                                        console.log('banner广告');
+                                        console.log('banner骞垮憡');
                                         return false;
                                     }
                                 }
                             }
                         }
                     } else if (cardType === 'cm_v2' && ['ad_web_s', 'ad_av', 'ad_web_gif', 'ad_player', 'ad_inline_3d', 'ad_inline_eggs'].includes(cardGoto)) {
-                        // ad_player大视频广告 ad_web_gif大gif广告 ad_web_s普通小广告 ad_av创作推广广告 ad_inline_3d  上方大的视频3d广告 ad_inline_eggs 上方大的视频广告
-                        console.log(`${cardGoto}广告去除)`);
+                        // ad_player澶ц棰戝箍鍛?ad_web_gif澶if骞垮憡 ad_web_s鏅€氬皬骞垮憡 ad_av鍒涗綔鎺ㄥ箍骞垮憡 ad_inline_3d  涓婃柟澶х殑瑙嗛3d骞垮憡 ad_inline_eggs 涓婃柟澶х殑瑙嗛骞垮憡
+                        console.log(`${cardGoto}骞垮憡鍘婚櫎)`);
                         return false;
                     } else if (cardType === 'small_cover_v10' && cardGoto === 'game') {
-                        console.log('游戏广告去除');
+                        console.log('娓告垙骞垮憡鍘婚櫎');
                         return false;
                     } else if (cardType === 'cm_double_v9' && cardGoto === 'ad_inline_av') {
-                        console.log('创作推广-大视频广告');
+                        console.log('鍒涗綔鎺ㄥ箍-澶ц棰戝箍鍛?);
                         return false;
                     }
                 } else {
                     console.log(`body:${$response.body}`);
-                    $notification.post(notifyTitle, '推荐页', "无card_type/card_goto");
+                    console.log(notifyTitle, '鎺ㄨ崘椤?, "鏃燾ard_type/card_goto");
                 }
                 return true;
             });
         }
     } else {
-        $notification.post(notifyTitle, "路径匹配错误:", url);
+        console.log(notifyTitle, "璺緞鍖归厤閿欒:", url);
     }
 }
 
@@ -117,7 +114,8 @@ $done({
 
 function fixPos(arr) {
     for (let i = 0; i < arr.length; i++) {
-        // 修复pos
+        // 淇pos
         arr[i].pos = i + 1;
     }
 }
+
